bffadbdac8ddd305e74c2460a2284e4898cd4cb8
Andrea Elizabeth Biju <58781287+AndreaEBiju@users.noreply.github.com>
2020-07-22 23:32:43 +0530

    Update warnings.js

--- warnings.js
+++ warnings.js
@@ -5,18 +5,87 @@ for(let i=0; i<input.length; i++){
 input[i].addEventListener("keyup", function(event) {
   // If "caps lock" is pressed, display the warning text
   if (event.getModifierState("CapsLock")) {
+    var text = document.createElement("P");   // Create a <button> element
+    text.innerHTML = "WARNING!Caps Lock is OFF!";                   // Insert text
+    text.style.color = "red";
+    input[i].insertAdjacentElement("afterend", text);               // Append <button> to <body>    
+    alert("Warning!Caps Lock is off!");
+  }
+});
+//fallback for other browsers
+input[i].attachEvent("keyup", function(event) {
+  // If "caps lock" is pressed, display the warning text
+  if (event.getModifierState("CapsLock")) {
     var text= "<p>WARNING!Caps lock is Off!</p>";
     input[i].insertAdjacentHTML("afterend",text);
     
     alert("Warning!Caps Lock is off!");
   }
-});}
+});
+}
 
 //warning for not loading video
 var video = document.getElementsByTagName("video");
 for(let i=0;i<video.length;i++){
     video[i].addEventListener("abort",abortFunction(event));
+    video[i].attachEvent("abort",abortFunction(event));
+    video[i].oncanplay = function() {
+      alert("Can start playing video");
+  };
+  video[i].onerror = function() {
+    alert("Error! Something went wrong");
+  };
+  video[i].onloadstart = function() {
+    alert("Starting to load video");
+  };
+  video[i].onseeked = function() {
+    alert("Seek operation completed!");
+  };
+  video[i].onstalled = function() {
+    alert("Media data is not available");
+  };
+  video[i].onsuspend = function() {
+    alert("Loading of the media is suspended");
+  };
+  video[i].onwaiting = function() {
+    alert("Wait! I need to buffer the next frame");
+  };
+  isSupp = video[i].canPlayType(vidType+';codecs="'+codType+'"');
+  if(isSupp==""){
+    alert("Your Browser doesn't support this media type!");
+  }
 }
 function abortFunction(event){
     alert("Video Loading Aborted!");
 }
+//for audio
+var audio = document.getElementsByTagName("audio");
+for(let i=0;i<audio.length;i++){
+    audio[i].addEventListener("abort",abortFunctionAudio(event));
+    audio[i].attachEvent("abort",abortFunctionAudio(event)); //fallback for IE
+    audio[i].oncanplay = function() {
+      alert("Can start playing audio");
+  };
+  audio[i].onerror = function() {
+    alert("Error! Something went wrong");
+  };
+  audio[i].onloadstart = function() {
+    alert("Starting to load audio");
+  };
+  audio[i].onseeked = function() {
+    alert("Seek operation completed!");
+  };
+  audio[i].onstalled = function() {
+    alert("Media data is not available");
+  };
+  audio[i].onsuspend = function() {
+    alert("Loading of the media is suspended");
+  };
+  audio[i].onwaiting = function() {
+    alert("Wait! I need to buffer the next frame");
+  };
+}
+function abortFunctionAudio(event){
+  alert("Audio Loading Aborted!");
+}
+
