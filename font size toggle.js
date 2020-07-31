5ddeedd0918d0d07aaeee7b30ace1c685aa4f498
Andrea Elizabeth Biju <58781287+AndreaEBiju@users.noreply.github.com>
2020-07-31 22:04:47 +0530

    final-under font and font size

--- font size toggle.js
+++ font size toggle.js
@@ -1,4 +1,4 @@
-//tested version
+//tested version for font size
 var btn2 = document.createElement("BUTTON");   // Create a <button> element
 btn2.innerHTML = "A-";                   // Insert text
 document.body.insertAdjacentElement("afterbegin", btn2);               // Append <button> to <body>
@@ -13,15 +13,36 @@ document.body.insertAdjacentElement("afterbegin", btn1);               // Append
 
 btn1.onclick=function btn1Click(){
     document.body.style.fontSize="150%";
-    alert("Font size increased");
 };
 
 btn2.onclick=function btn2Click(){
     document.body.style.fontSize="50%";
-    alert("Font size decreased");
 };
 
 btn3.onclick=function btn3Click(){
-		document.body.style.fontSize="100%";
-    alert("Font size original");
+	document.body.style.fontSize="100%";
+    setTimeout(function(){ alert("Font size brought back to original"); }, 3000);
 };
+
+var btn4 = document.createElement("BUTTON");   // Create a <button> element
+btn1.innerHTML = "Change Font";                   // Insert text
+document.body.insertAdjacentElement("afterbegin", btn4);               // Append <button> to <body>
+btn4.onclick= function(){
+    //code to set all body text from italics to normal and sans serif fonts for dyslexic
+    document.body.style.fontStyle = "normal";
+    document.body.style.fontFamily = "sans serif";
+
+    //code to set all address italics to normal for dyslexic
+    var address = document.getElementsByTagName("address");
+    for(let i=0; i<address.length; i++){
+        address[i].style.fontStyle = "normal";
+    }
+    var italics = document.getElementsByTagName("i");
+    for(let i=0; i<italics.length; i++){
+        italics[i].style.fontStyle = "normal";
+    }
+    var u = document.getElementsByTagName("u");
+    for(let i=0; i<u.length; i++){
+        u[i].style.fontStyle = "normal";
+    }
+}
