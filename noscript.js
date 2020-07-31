4e3edb06ef324e3657a540951dc2b5ce3f39fd73
Andrea Elizabeth Biju <58781287+AndreaEBiju@users.noreply.github.com>
2020-07-31 22:23:55 +0530

    noscript added-by default

---
+++ noscript.js
@@ -0,0 +1,7 @@
+var noscript = document.getElementsByTagName("noscript");
+if(noscript.length==0){
+    var noscr = document.createElement("noscript");
+    noscr.innerHTML = "You have not enabled JavaScript in your browser. Please enable it for a better experience of website features, and then refresh the page!";
+    noscr.style.fontSize = "larger";
+    noscr.style.fontWeight = "bolder";
+}
